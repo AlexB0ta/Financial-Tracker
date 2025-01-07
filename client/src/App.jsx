c48de41b635377e9744a2740ff2336bf52db03d7
AlexB0ota <alexbota950@yahoo.com>
2025-01-07 16:47:22 +0200

    added egld and logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,3 @@
-import {useState} from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Sidebar from "./components/Sidebar.jsx";
 import Home from "./pages/Home.jsx";
 import React from "react";
